c50c3cfcbd1df36a4e44c092c804e23d0564bf9f
Juan Martin Botero <jbotero@hotmail.com>
2024-09-06 10:39:58 -0400

    photo-gallery update sixth pass :(2)

--- JuanMartin.PhotoGallery/Program.cs
+++ JuanMartin.PhotoGallery/Program.cs
@@ -1,43 +1,58 @@
 using JuanMartin.PhotoGallery.Services;
 using JuanMartin.PhotoGallery.Controllers;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession(options =>
+namespace JuanMartin.PhotoGallery
 {
-    options.IdleTimeout = TimeSpan.FromSeconds(5); //  default is 20 minutes
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
-builder.Services.AddControllersWithViews();
-var implementationInstance = new PhotoService(builder.Configuration);
-builder.Services.AddSingleton<IPhotoService>(implementationInstance);
-builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+    public class Program
+    {
+		/// <summary>
+		/// Asp.net core applicatin entry point.
+		/// https://stackoverflow.com/questions/71705215/how-to-declare-global-variable-in-program-cs-and-use-it-in-controllers-in-net-6 
+		/// </summary>
+		/// <param name="args"></param>
+		public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-// Configure the HTTP request pipeline.
-var app = builder.Build();
+            // Add services to the container.
+            builder.Services.AddDistributedMemoryCache();
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(5); //  default is 20 minutes
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+            builder.Services.AddControllersWithViews();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    //        app.UseExceptionHandler("/Gallery/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+            var implementationInstance = new PhotoService(builder.Configuration);
+            builder.Services.AddSingleton<IPhotoService>(implementationInstance);
+            builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
+            // Configure the HTTP request pipeline.
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                //        app.UseExceptionHandler("/Gallery/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseSession();
+            app.UseAuthorization();
+            app.MapControllerRoute(
+                name: "default",
+    //    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Gallery}/{action=Index}/{id?}");
+            app.MapGet("/", (HttpContext context) => HttpUtility.InitializeSession(context.Session, builder.Configuration, context));
+            app.Run();
+        }
+    }
 }
-else
-{
-    app.UseDeveloperExceptionPage();
-}
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-app.UseSession();
-app.UseAuthorization();
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-//    pattern: "{controller=Gallery}/{action=Index}/{id?}");
-app.MapGet("/", (HttpContext context) => HttpUtility.InitializeSession(context.Session,builder.Configuration,context));
-app.Run();
